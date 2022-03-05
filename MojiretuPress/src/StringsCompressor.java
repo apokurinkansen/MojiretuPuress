f946c8c23ff544fd961766283404333943bab683
kubutaku <takuyuu0839@outlook.jp>
2022-03-05 13:39:13 +0900

    complite

--- MojiretuPress/src/StringsCompressor.java
+++ MojiretuPress/src/StringsCompressor.java
@@ -4,23 +4,21 @@ import java.util.List;
 public class StringsCompressor {
     private static final String TEXT = "AAAAABBBBBBBBBBBCCCCDDEFFFFGG";
     public static void main(String[] args) {
-        String text = encode(TEXT);
+        String text = Encode(TEXT);
         String decodeText = StringsExpender.Decode(text);
-
         System.out.println(text);
         System.out.println(decodeText);
     }
 
-    private static String encode(String encodeText) {
-        String tmp = String.valueOf(encodeText.charAt(0)); //A
-
+    private static String Encode(String encodeText) {
+        String encode = String.valueOf(encodeText.charAt(0));
         int count = 0;
         List<String> checkBox = new ArrayList<String>();
         StringBuilder checkText = new StringBuilder();
 
         for (int i = 0; i < encodeText.length(); i++) {
-            checkBox.add(String.valueOf(encodeText.charAt(i))); //A
-            if (checkBox.get(i).equals(tmp)) {
+            checkBox.add(String.valueOf(encodeText.charAt(i))); 
+            if (checkBox.get(i).equals(encode)) {
                 count++;
             } else {
                 checkText.append(checkBox.get(i - 1));
@@ -31,7 +29,7 @@ public class StringsCompressor {
                 checkText.append(checkBox.get(i));
                 checkText.append(count);
             }
-            tmp = checkBox.get(i);
+            encode = checkBox.get(i);
         }
         String ansText = checkText.toString();
         return ansText;
@@ -43,7 +41,7 @@ class StringsExpender {
         List<String> checkBox = new ArrayList<>();
         String abcText = "";
         StringBuilder intText = new StringBuilder();
-        StringBuilder ansText = new StringBuilder();
+        StringBuilder checkText = new StringBuilder();
         boolean startFlag = false;
                 
         for (int i = 0; i < decodeText.length(); i++) {
@@ -55,28 +53,18 @@ class StringsExpender {
             } else if (checkBox.get(i).matches("[^A-Z]")) {
                 intText.append(checkBox.get(i));
                 startFlag = true;
-                //Debug
-                //System.out.println(intText);
             }
             if (startFlag == true) {
                 String converText = intText.toString();
-                //System.out.println("convertText(String):" + converText);
-                //convertTextにintTextのstring変換完了
                 int count = 0;
                 count = Integer.parseInt(converText);
-                //countにconvertTextのint型に変換
-                //System.out.println("count(int):" + count);
-
                 for (int j = 0; j < count; j++) {
-                    ansText.append(abcText);
-                    //Debug
-                    //System.out.println(ansText);
+                    checkText.append(abcText);
                 }
                 startFlag = false;
             }
         }
-        String returnAnsText = ansText.toString();
-        //System.out.println("checkText:"+ checkText);
-        return returnAnsText;
+        String ansText = checkText.toString();
+        return ansText;
     }
 }
